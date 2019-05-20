df370737d2dab5c744d3f136f83e26c5244920c6
Eric Marchand <eric.marchand@4d.com>
2019-05-20 10:50:32 +0200

    Update swift package file

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "XcodeProjKit",
+    products: [
+        .library(
+            name: "XcodeProjKit",
+            targets: ["XcodeProjKit"]
+        )
+    ],
     targets: [
         .target(name: "XcodeProjKit", path: "Sources"),
         .testTarget(name: "Tests", dependencies: ["XcodeProjKit"], path: "Tests")
