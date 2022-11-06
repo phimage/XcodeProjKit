fceac2b2b82cc8e9a1cfabb13d1d7ff3d12d8d04
Eric Marchand <phimage@users.noreply.github.com>
2022-11-06 01:33:43 +0100

    Allow to modify build settings

--- Sources/PBXBuildStyle.swift
+++ Sources/PBXBuildStyle.swift
@@ -17,7 +17,14 @@ public class PBXBuildStyle: PBXProjectItem {
     #if LAZY
     public lazy var buildSettings: [String: Any]? = dictionary(PBXKeys.buildSettings)
     #else
-    public var buildSettings: [String: Any]? { dictionary(PBXKeys.buildSettings) }
+    public var buildSettings: [String: Any]? { 
+        get {
+            dictionary(PBXKeys.buildSettings)
+        }
+        set {
+            self.fields[PBXKeys.buildSettings.rawValue] = newValue
+        }
+    }
     #endif
 
 }
